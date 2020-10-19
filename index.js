7d65d6b8a2f4e6ab4d3068e9cb2f1628b4518718
Wyyann <66358109+Wyyann@users.noreply.github.com>
2020-10-19 22:01:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,25 +13,7 @@ client.on("message", function(msg) {
 
     var args = msg.content.split(" ");
     var cmd = args.shift().toLowerCase();
-
-    require("./commands/help.js").message(discord, client, msg, args, cmd);
-
-    require("./commands/information/apply.js").message(discord, client, msg, args, cmd);
-    require("./commands/information/discord.js").message(discord, client, msg, args, cmd);
-    require("./commands/information/ip.js").message(discord, client, msg, args, cmd);
-    require("./commands/information/twitter.js").message(discord, client, msg, args, cmd);
-    require("./commands/information/website.js").message(discord, client, msg, args, cmd);
-    require("./commands/information/youtube.js").message(discord, client, msg, args, cmd);
-
-    require("./commands/staff/ban.js").message(discord, client, msg, args, cmd);
-    require("./commands/staff/kick.js").message(discord, client, msg, args, cmd);
-    require("./commands/staff/purge.js").message(discord, client, msg, args, cmd);
-    require("./commands/staff/mute.js").message(discord, client, msg, args, cmd);
-    require("./commands/staff/unmute.js").message(discord, client, msg, args, cmd);
-
-    require("./commands/staff/accept.js").message(discord, client, msg, args, cmd);
-    require("./commands/staff/decline.js").message(discord, client, msg, args, cmd);
-
+    
     require("./features/bumpReminder.js").message(discord, client, msg, args, cmd);
 
 
