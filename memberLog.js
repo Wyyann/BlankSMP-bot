c0d1180465827833d3a846be2efad0ac1917d11f
Wyyann <66358109+Wyyann@users.noreply.github.com>
2021-03-01 11:07:08 +0000

    Update memberLog.js

--- memberLog.js
+++ memberLog.js
@@ -1,8 +1,8 @@
-var memberLogChannel = "767854489247154198";
+var memberLogChannel = "815902581083734066";
 
 module.exports = {guildMemberAdd: function(discord, client, member) {
 
-    client.channels.cache.get(memberLogChannel).send("<@" + member.id + "> has joined. :open_hands:");
+    client.channels.cache.get(memberLogChannel).send("Welcome <@" + member.id + ">! If you wish to join our server, please check #<#751059378722832404> and search for our application!");
 
 }, guildMemberRemove: function(discord, client, member) {
 
