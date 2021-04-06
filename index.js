1c28cbc8590157afda05b0e0fde8bbb101f09243
Wyyann <66358109+Wyyann@users.noreply.github.com>
2021-04-06 09:33:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -50,6 +50,24 @@ client.on('message', message => {
         })
 
     }
+    var botInteractionsChannel = "751062855880802324";
+    
+        if(msg.author.id == "302050872383242240") { // DISBOARD
+        if(msg.embeds[0]) {
+            if(msg.embeds[0].description) {
+                if(msg.embeds[0].description.includes("Bump done")) {
+                    setTimeout(function() {
+                        // ping
+                        client.channels.cache.get(botInteractionsChannel).send("<@271910522058702850>, <@319456708495802369>").then(function(pingMsg) {
+                            pingMsg.delete();
+                        });
+
+                        // reminder
+                        client.channels.cache.get(botInteractionsChannel).send(new discord.MessageEmbed()
+                            .setTitle("Bump Reminder")
+                            .setDescription("This server can now be bumped again.")
+                            .setTimestamp());
+                    }, 1000 * 60 * 60 * 2);
 
     //!report
     if(message.content.startsWith(`${prefix}report`)) {
